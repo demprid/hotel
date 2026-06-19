293aa2ce38f13a7e0e249760c18e7f8b2dc00f2a
dp <sjbn4sndz7@privaterelay.appleid.com>
2026-06-19 14:43:31 +0300

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
 // ============================================================
 
 // URL вашего веб-приложения Google Apps Script
-const GOOGLE_SHEETS_URL = 'https://script.google.com/macros/s/AKfycbzAyfDWBIZJTjtbh84aF9b2GlqI6jMCMObHymkSf2nwlEmznT-IIqpur9EXMlGiEuesEg/exec';
+const GOOGLE_SHEETS_URL = 'https://script.google.com/macros/s/AKfycbwWgwaSSTYDDHFv0fSqbTIlh2qcmdxSw5p7McW_NrwIKK8wFd8SL9ixuTAnrah76jm3/exec';
 
 // Название страницы квитанции (не меняйте, если не переименовывали файл)
 const RECEIPT_PAGE = 'receipt.html';
