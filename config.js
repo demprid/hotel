56543a5aa84d21cfbe02388dad4ef0f72e12e3d0
dp <sjbn4sndz7@privaterelay.appleid.com>
2026-06-23 17:16:20 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,2 @@
-// ============================================================
-// НАСТРОЙКИ ПРОЕКТА
-// ============================================================
-
-// URL вашего веб-приложения Google Apps Script
-const GOOGLE_SHEETS_URL = 'https://script.google.com/macros/s/AKfycbztjmWn2JTGDhhWjzEIiAI4XEcpaFcPzJQ7lXXWMfAwLxAmH1EOFOWtIwvVKMZ_KqjZYw/exec';
-
-// Название страницы квитанции (не меняйте, если не переименовывали файл)
+const GOOGLE_SHEETS_URL = 'https://script.google.com/macros/s/AKfycbwZ2_KAodATQsIar8CvaaJSnI1JGN0oOvortilVeQyVoKuqGvd1FY2A07uJJsUVPP2NAw/exec';
 const RECEIPT_PAGE = 'receipt.html';
-
-// Другие настройки (при необходимости)
-const APP_NAME = 'Отель ДП | Введенская слобода';
-const SESSION_HOURS = 0.25; // срок действия сессии в часах
