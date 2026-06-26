ffbb4a7aced7544357f168d18781fe7929e3856f
dp <sjbn4sndz7@privaterelay.appleid.com>
2026-06-26 08:33:17 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-const GOOGLE_SHEETS_URL = 'https://script.google.com/macros/s/AKfycbzvkiPMCPzhtUpiHzJaxAjM4-FLVjJeP_jpNjjhdmlTOWDpz1X2s8vx6MT3EsOz9EjLdQ/exec';
+const GOOGLE_SHEETS_URL = 'https://script.google.com/macros/s/AKfycbwR5YeUftlnCbl0pd1UmzGQ4gWU-9uTPeQK4Gax8tNQnGM0LHtGbJZplXX8iG1BAjONgw/exec';
 const RECEIPT_PAGE = 'receipt.html';
